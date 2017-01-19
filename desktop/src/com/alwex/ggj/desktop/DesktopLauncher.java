9d8c8ee4c7312bb7bacab6ad8fbccc7eb7f25c70
Alexandre Guidet <alex@room9.co.nz>
2017-01-20 09:22:31 +1300

    base libgdx install + artemis and tween engine

---
+++ desktop/src/com/alwex/ggj/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alwex.ggj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alwex.ggj.JamGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JamGame(), config);
+	}
+}
