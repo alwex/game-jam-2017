e14b5c2a167f27dbcff042d53a7c621dc31ff5d3
GUIDET Alexandre <guidet.alexandre@gmail.com>
2017-01-20 20:53:31 +1300

    added a few systems

--- core/src/com/alwex/ggj/screens/LevelScreen.java
+++ core/src/com/alwex/ggj/screens/LevelScreen.java
@@ -1,7 +1,11 @@
 package com.alwex.ggj.screens;
 
 import com.alwex.ggj.JamGame;
+import com.alwex.ggj.components.PositionComponent;
+import com.alwex.ggj.components.ShapeComponent;
+import com.alwex.ggj.systems.PositionSystem;
 import com.alwex.ggj.systems.RenderSystem;
+import com.artemis.Entity;
 import com.artemis.World;
 import com.artemis.WorldConfiguration;
 import com.artemis.WorldConfigurationBuilder;
@@ -35,7 +39,8 @@ public class LevelScreen implements Screen {
                         new EventSystem(),
 
                         // other systems goes here
-                        new RenderSystem(batch, camera)
+                        new RenderSystem(batch, camera),
+                        new PositionSystem()
                 ).build();
 
         world = new World(config);
@@ -43,7 +48,17 @@ public class LevelScreen implements Screen {
 
     @Override
     public void show() {
+        Entity e = world.createEntity()
+                .edit()
+                .add(new PositionComponent(10, 10))
+                .add(new ShapeComponent(10, 10))
+                .getEntity();
 
+        Entity e2 = world.createEntity()
+                .edit()
+                .add(new ShapeComponent(100, 100))
+                .add(new PositionComponent(100, 300))
+                .getEntity();
     }
 
     @Override
@@ -52,8 +67,6 @@ public class LevelScreen implements Screen {
         world.setDelta(delta);
         world.process();
 
-        Gdx.gl.glClearColor(1, 1, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
 //        batch.draw(img, 0, 0);
         batch.end();
--- core/src/com/alwex/ggj/systems/RenderSystem.java
+++ core/src/com/alwex/ggj/systems/RenderSystem.java
@@ -1,23 +1,36 @@
 package com.alwex.ggj.systems;
 
 import com.alwex.ggj.components.PositionComponent;
+import com.alwex.ggj.components.ShapeComponent;
 import com.alwex.ggj.components.SpriteComponent;
 import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
 import com.artemis.Entity;
+import com.artemis.annotations.Wire;
 import com.artemis.systems.EntityProcessingSystem;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 /**
  * Created by alexandreguidet on 20/01/17.
  */
+@Wire
 public class RenderSystem extends EntityProcessingSystem {
 
+    ComponentMapper<PositionComponent> positionMapper;
+    ComponentMapper<ShapeComponent> shapeMapper;
+
+
     SpriteBatch batch;
+    ShapeRenderer shapeRenderer;
     OrthographicCamera camera;
 
     public RenderSystem(SpriteBatch batch, OrthographicCamera camera) {
-        super(Aspect.all(PositionComponent.class, SpriteComponent.class));
+        super(Aspect.all(PositionComponent.class, ShapeComponent.class));
 
         this.batch = batch;
         this.camera = camera;
@@ -25,23 +38,35 @@ public class RenderSystem extends EntityProcessingSystem {
 
     @Override
     protected void initialize() {
-
+        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
     protected void begin() {
 
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
+//        batch.setProjectionMatrix(camera.combined);
+//        batch.begin();
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.YELLOW);
     }
 
     @Override
     protected void process(Entity e) {
 
+        PositionComponent p = positionMapper.get(e);
+        ShapeComponent shape = shapeMapper.get(e);
+
+        shapeRenderer.rect(p.x, p.y, shape.width, shape.height);
     }
 
     @Override
     protected void end() {
-        batch.end();
+//        batch.end();
+        shapeRenderer.end();
     }
 }
