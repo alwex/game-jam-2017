15ad968466de7036b384566f7e02654ea186ecb5
GUIDET Alexandre <guidet.alexandre@gmail.com>
2017-01-21 16:21:58 +1300

    added ambient sound

--- core/src/com/alwex/ggj/systems/SoundSystem.java
+++ core/src/com/alwex/ggj/systems/SoundSystem.java
@@ -4,6 +4,7 @@ import com.alwex.ggj.events.SlicedEvent;
 import com.artemis.BaseSystem;
 import com.artemis.annotations.Wire;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.math.MathUtils;
 import net.mostlyoriginal.api.event.common.Subscribe;
@@ -15,6 +16,7 @@ import net.mostlyoriginal.api.event.common.Subscribe;
 public class SoundSystem extends BaseSystem {
 
     AssetManager assetManager;
+    Music ambient;
 
     public SoundSystem(AssetManager assetManager) {
         this.assetManager = assetManager;
@@ -25,8 +27,15 @@ public class SoundSystem extends BaseSystem {
 
     }
 
+    @Override
+    protected void initialize() {
+        ambient = assetManager.get("sounds/ambient.ogg", Music.class);
+        ambient.setLooping(true);
+        ambient.play();
+    }
+
     @Subscribe
     public void onSliced(SlicedEvent event) {
-        assetManager.get("sounds/splash.mp3", Sound.class).play(1, MathUtils.random(0.5f, 1f), 0);
+        assetManager.get("sounds/splash.mp3", Sound.class).play(1, MathUtils.random(0.8f, 1f), 0);
     }
 }
