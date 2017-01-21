8febf6235f501b552ee7d347e34f1bbc6cfad5c4
Joshua Brungar <jbrungar@Joshuas-MacBook-Pro.local>
2017-01-21 13:16:49 +1300

    Fish slicing improved. Spawning event added.

--- core/src/com/alwex/ggj/systems/SpawnSystem.java
+++ core/src/com/alwex/ggj/systems/SpawnSystem.java
@@ -1,23 +1,34 @@
 package com.alwex.ggj.systems;
 
-import com.artemis.Aspect;
+import com.alwex.ggj.events.SpawnEvent;
+import com.artemis.BaseSystem;
 import com.artemis.annotations.Wire;
-import com.artemis.systems.IntervalEntitySystem;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import net.mostlyoriginal.api.event.common.EventSystem;
+import net.mostlyoriginal.api.event.common.Subscribe;
 
 /**
  * Created by jbrungar on 21/01/17.
  */
 @Wire
-public class SpawnSystem extends IntervalEntitySystem {
+public class SpawnSystem extends BaseSystem {
 
+    EventSystem eventSystem;
     FishSystem fishSystem;
 
-    public SpawnSystem(float interval) {
-        super(Aspect.all(), interval);
+    public SpawnSystem() {
     }
 
     @Override
     protected void processSystem() {
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            eventSystem.dispatch(new SpawnEvent());
+        }
+    }
+
+    @Subscribe
+    public void spawnEventListener(SpawnEvent event) {
         fishSystem.spawn();
     }
 }
